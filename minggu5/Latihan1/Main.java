6d7427fef49e1b9c5659219dfc7b80595d52325d
nanda <nandawijaya@apps.ipb.ac.id>
2022-09-19 15:54:24 +0700

    add Latihan 1

---
+++ minggu5/Latihan1/Main.java
@@ -0,0 +1,12 @@
+package Latihan1;
+
+public class Main {
+    public static void main(String[] args) {
+        Titik titik1 = new Titik();
+        System.out.println(titik1.hitungTitik(6, 9));
+        System.out.println(titik1.hitungTitik(6, 9, 13));
+        System.out.println(titik1.hitungTitik("6", "9"));
+    
+    }
+    
+}
---
+++ minggu5/Latihan1/Titik.java
@@ -0,0 +1,17 @@
+package Latihan1;
+
+public class Titik {
+    public int hitungTitik(int a, int b){
+        int hasil = a + b;
+        return hasil;
+    }
+    public  int hitungTitik(int a, int b, int c){
+        int hasil = a + b + c;
+        return hasil;
+    }
+    public String hitungTitik(String a, String b){
+        String hasil = a + b;
+        return hasil;
+    }
+    
+}
