43997625752df3c0e8327006a343ec4b15939271
nanda <nandawijaya@apps.ipb.ac.id>
2022-11-14 14:09:35 +0700

    fix tugas minggu 9

--- minggu9/Tugas/Test.java
+++ minggu9/Tugas/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         Fans fans1 = new Fans();
         Fans fans2 = new Fans("Mona");
-        Fans fans3 = new Fans("Tomi");
+        Fans fans3 = new KpopFans("Tomi");
         KpopFans fans4 = new KpopFans("Febi");
 
         fans1.watchingPerformance();
